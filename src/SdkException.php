99baf4bb1adfae6577f21d26407d23a9c80043d4
andrew <boobaleh15@yandex.ru>
2017-10-20 00:05:41 +0300

    Добавил метод получения токена я.Метрики

---
+++ src/SdkException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 16.10.17
+ * Time: 0:06
+ */
+
+namespace SeoBroweb\YandexMetrikaApi;
+
+
+class SdkException extends \Exception
+{
+
+}
